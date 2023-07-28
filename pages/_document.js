244b50cf6816801c7680f47d060f4d13b69dc41c
Gowtham S <gowtham54321@yahoo.com>
2023-07-28 12:27:44 -0400

    optimisations

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,35 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document"
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <script
+                        async
+                        src="https://www.googletagmanager.com/gtag/js?id=G-9LZWL9VNXH"
+                    ></script>
+                    <script>
+                        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9LZWL9VNXH');
+            `}
+                    </script>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
+
+export default MyDocument
